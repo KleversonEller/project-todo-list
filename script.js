1e4cee534479a548142c6dd6a8d3d33809042fb2
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-20 15:24:07 -0300

    Requisitos 5 e 6 - implementando tarefas na lista

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  //? Requisito 5 - 6
+
+  let botaoCriaTarefas = document.querySelector('#criar-tarefa');
+
+  botaoCriaTarefas.addEventListener('click', function () {
+    let textoTarefa = document.querySelector('#texto-tarefa');
+    let lista = document.querySelector('#lista-tarefas');
+    let tarefa = document.createElement('li');
+    tarefa.innerText = textoTarefa.value;
+    lista.appendChild(tarefa);
+    textoTarefa.value = '';
+  });
+
+  //! Implementa um o texto digitado no input na lista de tarefas em ordem de criação e apaga o texto do input
+};
