64578c6ccaa2866fd8e7178266adb0834c39cbe4
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-20 15:52:00 -0300

    Requisito 8 - pinta o fundo apenas do elemento clicado

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ window.onload = function () {
 
   //! Implementa um o texto digitado no input na lista de tarefas em ordem de criação e apaga o texto do input
 
-  //? Requisito 7
+  //? Requisito 7 - 8
 
   let lista = document.querySelector('#lista-tarefas');
 
   lista.addEventListener('click', function (evento) {
+    let tarefas = document.querySelectorAll('#lista-tarefas li');
+    for (let posicao of tarefas) {
+      posicao.style.backgroundColor = 'white';
+    }
     let tarefa = evento.target;
     tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 
-  //! Ao clicar em uma tarefa da lista pinta o fundo
+  //! Ao clicar em uma tarefa da lista pinta o fundo apenas do elemento clicado
 };
