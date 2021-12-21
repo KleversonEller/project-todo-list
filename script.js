20be3eb63084ab0604cfaea0984a05f3e8b0b22b
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-20 22:08:39 -0300

    Restruturando script

--- script.js
+++ script.js
@@ -1,4 +1,74 @@
 window.onload = function () {
+  //? Requisito 5 - 6
+
+  let botaoCriaTarefas = document.querySelector('#criar-tarefa');
+
+  botaoCriaTarefas.addEventListener('click', function () {
+    let textoTarefa = document.querySelector('#texto-tarefa');
+    let lista = document.querySelector('#lista-tarefas');
+    let tarefa = document.createElement('li');
+    tarefa.innerText = textoTarefa.value;
+    lista.appendChild(tarefa);
+    textoTarefa.value = '';
+  });
+
+  //! Implementa um o texto digitado no input na lista de tarefas em ordem de criação e apaga o texto do input
+
+  //? Requisito 7 - 8
+
+  let lista = document.querySelector('#lista-tarefas');
+
+  lista.addEventListener('click', function (evento) {
+    let tarefas = document.querySelectorAll('#lista-tarefas li');
+    for (let posicao of tarefas) {
+      posicao.removeAttribute('id');
+      evento.target.id = 'select';
+    }
+  });
+
+  //! Ao clicar em uma tarefa da lista pinta o fundo apenas do elemento clicado
+
+  //? Requisito 9
+
+  lista.addEventListener('dblclick', function (evento) {
+    let tarefa = evento.target;
+    if (tarefa.classList == 'completed') {
+      tarefa.removeAttribute('class');
+    } else {
+      tarefa.classList.add('completed');
+    }
+  });
+
+  //! Ao clicar duas vezes em uma tarefa da lista adiciona a classe completed
+
+  //? Requisito 10
+
+  let botaoApagarLista = document.querySelector('#apaga-tudo');
+
+  botaoApagarLista.addEventListener('click', function () {
+    let tarefas = document.querySelectorAll('#lista-tarefas li');
+    for (let posicao of tarefas) {
+      lista.removeChild(posicao);
+    }
+  });
+
+  //! Implementa botao apaga lista
+
+  //? Requisito 11
+
+  let botaoApagarCompletas = document.querySelector('#remover-finalizados');
+
+  botaoApagarCompletas.addEventListener('click', function () {
+    let tarefas = document.querySelectorAll('#lista-tarefas li');
+    for (let posicao of tarefas) {
+      if (posicao.classList == 'completed') {
+        lista.removeChild(posicao);
+      }
+    }
+  });
+
+  //! Implementa botao apaga tarefas completas
+
   //? Requisito 12
 
   let botaoSalvarLista = document.querySelector('#salvar-tarefas');
@@ -37,85 +107,16 @@ window.onload = function () {
   itensSalvos();
 
   //! Salva a lista e retorna a lista salva quando a pagina for recarregada
+
+  //? Requisito 14
+
+  let botaoRemoveSelecionado = document.querySelector('#remover-selecionado');
+
+  botaoRemoveSelecionado.addEventListener('click', function () {
+    let selecionado = document.querySelector('#select');
+
+    lista.removeChild(selecionado);
+  });
+
+  //! Implementa botao que apaga tarefas selecionadas
 };
-//? Requisito 5 - 6
-
-let botaoCriaTarefas = document.querySelector('#criar-tarefa');
-
-botaoCriaTarefas.addEventListener('click', function () {
-  let textoTarefa = document.querySelector('#texto-tarefa');
-  let lista = document.querySelector('#lista-tarefas');
-  let tarefa = document.createElement('li');
-  tarefa.innerText = textoTarefa.value;
-  lista.appendChild(tarefa);
-  textoTarefa.value = '';
-});
-
-//! Implementa um o texto digitado no input na lista de tarefas em ordem de criação e apaga o texto do input
-
-//? Requisito 7 - 8
-
-let lista = document.querySelector('#lista-tarefas');
-
-lista.addEventListener('click', function (evento) {
-  let tarefas = document.querySelectorAll('#lista-tarefas li');
-  for (let posicao of tarefas) {
-    posicao.removeAttribute('id');
-    evento.target.id = 'select';
-  }
-});
-
-//! Ao clicar em uma tarefa da lista pinta o fundo apenas do elemento clicado
-
-//? Requisito 9
-
-lista.addEventListener('dblclick', function (evento) {
-  let tarefa = evento.target;
-  if (tarefa.classList == 'completed') {
-    tarefa.removeAttribute('class');
-  } else {
-    tarefa.classList.add('completed');
-  }
-});
-
-//! Ao clicar duas vezes em uma tarefa da lista adiciona a classe completed
-
-//? Requisito 10
-
-let botaoApagarLista = document.querySelector('#apaga-tudo');
-
-botaoApagarLista.addEventListener('click', function () {
-  let tarefas = document.querySelectorAll('#lista-tarefas li');
-  for (let posicao of tarefas) {
-    lista.removeChild(posicao);
-  }
-});
-
-//! Implementa botao apaga lista
-
-//? Requisito 11
-
-let botaoApagarCompletas = document.querySelector('#remover-finalizados');
-
-botaoApagarCompletas.addEventListener('click', function () {
-  let tarefas = document.querySelectorAll('#lista-tarefas li');
-  for (let posicao of tarefas) {
-    if (posicao.classList == 'completed') {
-      lista.removeChild(posicao);
-    }
-  }
-});
-
-//! Implementa botao apaga tarefas completas
-
-//? Requisito 14
-
-let botaoRemoveSelecionado = document.querySelector('#remover-selecionado');
-
-botaoRemoveSelecionado.addEventListener('click', function () {
-  let selecionado = document.querySelector('#select');
-
-  lista.removeChild(selecionado);
-});
-
-//! Implementa botao que apaga tarefas selecionadas
